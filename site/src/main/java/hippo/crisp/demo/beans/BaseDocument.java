8e42b25fff19cbd294ff8e856b76097dfaed9837
jgreelyBR <jason.greely@bloomreach.com>
2018-03-12 12:41:02 -0400

    og push

---
+++ site/src/main/java/hippo/crisp/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package hippo.crisp.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippocrispdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
